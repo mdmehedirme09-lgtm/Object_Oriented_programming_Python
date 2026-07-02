3be0ea3c05d2caa4f6e3618495d4707ec5e323fc
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-07-02 10:56:58 +0600

    add new

---
+++ List_remove_element.py
@@ -0,0 +1,18 @@
+a=[1,2,3,4]
+a.remove(2)
+print(a)
+
+a.pop()
+print(a)
+
+b=[1,2,3,4,5,6]
+b.pop(2)#remove index 2
+print(b)
+
+del b[1]#delete the index mentioned
+print(b)
+
+
+#remove all
+b.clear()
+print(b)
