994960a22566bf44e45c13b8852f71820a16501f
Md Mehedi Hasan <mdmahadehasan4601@gmail.com>
2026-07-03 11:16:29 +0600

    add

---
+++ print_staement.py
@@ -0,0 +1,24 @@
+name="mehedi" 
+age=21
+
+#using commas
+print(name)
+print("Age",age,"Name:",name)
+#using a new line
+print("Age",age,"\nName:",name)
+#using +
+print("Name: "+name +" Age: "+str(age))
+#using f-string
+print(f"Name:{name} Age:{age}")
+print(f"My name is {name}")
+
+#using format
+print("My name is {}".format(name))
+print("My name is {} & My age is {}".format(name,age))
+
+#addition
+
+x=10
+y=20
+
+print(f"Sum is {x+y}")
